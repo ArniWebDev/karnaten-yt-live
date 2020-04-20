04001a7dfc3f9a2798983d790de682be61a26e31
Arnold <arniwebdev@gmail.com>
2020-04-20 21:15:39 +0300

    Feladat 2. resz

--- Feladatok/GondoltamEgySzamra/app.js
+++ Feladatok/GondoltamEgySzamra/app.js
@@ -1,27 +1,44 @@
-const difficultyList = [
-  {
-    difName: "Easy",
-    difDescription: "Easy Leiras",
-    try: 10,
-  },
-  {
-    difName: "Medium",
-    difDescription: "Medium Leiras",
-    try: 5,
-  },
-  {
-    difName: "Hard",
-    difDescription: "Hard Leiras",
-    try: 1,
-  },
-];
+import { difficultyList } from "./data.js";
 
-const easyBtn = document.querySelector(".general-container button");
-const mediumBtn = document.querySelector(".general-container button");
-const hardBtn = document.querySelector(".general-container button");
+let inputValue = 0;
+let tryNumber = 10;
 
-const checkBtn = document.querySelector(".general-container button");
+const checkBtn = document.querySelector(".input-container button");
+const numberInput = document.querySelector(".input-container input");
+const navigation = document.querySelector(".nav");
+const description = document.querySelector(".general-container p");
+const startBtn = document.querySelector(".start-btn");
 
-easyBtn.addEventListener("click", function () {
-  console.log("Easy BTN");
+const getRandomInt = () => {
+  return Math.floor(Math.random() * Math.floor(100));
+};
+
+difficultyList.forEach((elem) => {
+  const btn = document.createElement("button");
+  btn.innerText = elem.name;
+  btn.classList.add(elem.class);
+  btn.addEventListener("click", function () {
+    description.innerText = elem.description;
+    tryNumber = elem.try;
+    startBtn.classList.add("activate-click");
+  });
+  navigation.appendChild(btn);
+});
+
+startBtn.addEventListener("click", function () {
+  const myNumber = getRandomInt();
+  checkBtn.classList.add("activate-click");
+  startBtn.classList.add("hide");
+  console.log(myNumber);
+  for (let i = 0; i < tryNumber; i++) {
+    if (inputValue === 15) {
+      break;
+    }
+  }
+});
+
+checkBtn.addEventListener("click", function () {
+  console.log("check click");
+  inputValue = numberInput.value ? parseInt(numberInput.value) : 0;
+  console.log(inputValue);
 });
---
+++ Feladatok/GondoltamEgySzamra/data.js
@@ -0,0 +1,22 @@
+const difficultyList = [
+  {
+    name: "Easy",
+    description: "Easy Leiras",
+    try: 10,
+    class: "easy",
+  },
+  {
+    name: "Medium",
+    description: "Medium Leiras",
+    try: 5,
+    class: "medium",
+  },
+  {
+    name: "Hard",
+    description: "Hard Leiras",
+    try: 1,
+    class: "hard",
+  },
+];
+
+export { difficultyList };
